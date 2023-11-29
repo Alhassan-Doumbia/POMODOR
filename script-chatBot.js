d659b2e5a308cec185d71ee9c8cd35348ba85e9b
Doumbia Alhassan <hassan4cod@gmail.com>
2023-11-29 11:35:55 +0000

    resolution du problème de retour à la ligne avec  flexshrink:1

--- script-chatBot.js
+++ script-chatBot.js
@@ -1,32 +1,31 @@
+const chatContainer = document.querySelector(".chatContainer");
+const chat = document.querySelector(".chat"); // le ul contenant les bulles de chat
+const userInputField = document.querySelector(".userInputField");
+const sendIcon = document.querySelector(".sendIcon");
 
-const chatContainer=document.querySelector(".chatContainer");
-const chat=document.querySelector(".chat")//le ul conttenant les bulles de chat
-const userInput=document.querySelector(".userInputField").value;
-const userTextArea=document.querySelector(".userInputField")
-const sendinIcon=document.querySelector(".sendinIcon");
-//creer une bulle de message : 
-function newMessage(){
-    //le nerf de guerre c'est ce bail
-    const message=document.createElement("li");
-    const userInputField=document.getElementsByClassName("userInputField")
-    message.innerText=userInput;
+// créer une bulle de message :
+function newMessage() {
+    const message = document.createElement("li");
+    const input=userInputField.value;
+    const userInput =`<p>${input}</p>`;
+    message.innerHTML=userInput;//userInputField.value;
     chat.appendChild(message);
-    //pour soucis de debuggage 
-    console.log("ajout du nouveau message en appuyant sur la touce entrée ou sur le logo ")
-    //pour vider le bail
-    userInputField.value = "";
-    return false; 
-}
-//utilisation de l'icone d'envoit pour envoyer des requêtes
-sendinIcon.addEventListener('click',newMessage)
-//utilisation de la touche entrée 
-userInputField.addEventListener('keypress',(e)=>{
-    if (e.which==13 ||e.keyCode==13 && userInput!=""){
-        // e.preventDefault();
-        newMessage();
-    }
-    else{
-        console.log("empty message , not displayed");
-    }
-})
 
+    // pour des raisons de débogage
+    console.log("Ajout du nouveau message en appuyant sur la touche Entrée ou sur le logo.");
+ 
+    // pour vider le champ de saisie
+    userInputField.value = "";
+    return false;
+}
+// utilisation de l'icône d'envoi pour envoyer des requêtes
+sendIcon.addEventListener('click', newMessage);
+
+// utilisation de la touche Entrée
+userInputField.addEventListener('keypress', (e) => {
+    if ((e.which == 13 || e.keyCode == 13) && userInputField.value!== "") {
+        newMessage();
+    } else {
+        console.log("Message vide, non affiché.");
+    }
+});
