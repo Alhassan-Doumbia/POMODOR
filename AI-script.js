94a80963e24935043acd9b9e8ea6f152199f3c8a
Doumbia Alhassan <hassan4cod@gmail.com>
2023-11-29 11:40:21 +0000

    Update README.md
    ###Followup:
    Why did you include the line "go to this link : https://pomodoro.io" in the diff?
    
    ###Response: (generated by blackbox.ai )

--- AI-script.js
+++ AI-script.js
@@ -1,3 +1,35 @@
 // c'est ici qu'on intégrera le code de l'API de chat GPT et comment la zone d'IA sera géré 
 //il sera donc directement lié à la page Web 
 //peut être aussi on creer un whole fichier JS où il yaura les différents scripts 
+
+
+const chatContainer=document.querySelector(".chatContainer");
+const chat=document.querySelector(".chat")//le ul conttenant les bulles de chat
+const userInput=document.querySelector(".userInputField").value;
+const userTextArea=document.querySelector(".userInputField")
+const sendinIcon=document.querySelector(".sendinIcon");
+//creer une bulle de message : 
+function newMessage(){
+    //le nerf de guerre c'est ce bail
+    const message=document.createElement("li");
+    const userInputField=document.getElementsByClassName("userInputField")
+    message.innerText=userInput;
+    chat.appendChild(message);
+    //pour soucis de debuggage 
+    console.log("ajout du nouveau message en appuyant sur la touce entrée ou sur le logo ")
+    //pour vider le bail
+    userInputField.value = "";
+    return false; 
+}
+//utilisation de l'icone d'envoit pour envoyer des requêtes
+sendinIcon.addEventListener('click',newMessage)
+//utilisation de la touche entrée 
+userInputField.addEventListener('keypress',(e)=>{
+    if (e.which==13 ||e.keyCode==13 && userInput!=""){
+        // e.preventDefault();
+        newMessage();
+    }
+    else{
+        console.log("empty message , not displayed");
+    }
+})
--- script-test.js
+++
@@ -1,7 +0,0 @@
-const body=document.querySelector("body");
-
-body.addEventListener("keypress",(e)=>{
-    if(e.keyCode==13){
-        console.log("touche entrée")
-    }
-})
